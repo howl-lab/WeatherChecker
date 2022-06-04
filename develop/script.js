8f01d867154fe65018cf9c3d99ee4ce395b0aabd
Boo <cottage@boos-mac-mini.lan>
2022-06-04 00:08:40 -0700

    initial code files

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,19 @@
 // search button
 let searchBtn = $('#searchBtn');
+// city htmlEl
 let cityEl = $('#city-value');
+// temp htmlEl
+let tempEl = $('#temp-value');
+// wind htmlEl
+let windEl = $('#wind-value');
+// humidity htmlEl
+let humidityEl = $('#humidity-value');
+// uv index htmlEl
+let uviEl = $('#uvi-value');
+
+//current day
+let currentDay = moment().format(' DD/MM/YYYY');//capital 24 hour
+
 // 5 day forecast
 let searchHistory = JSON.parse(localStorage.getItem('history')) || [];
 console.log(searchHistory);
@@ -8,11 +21,16 @@ console.log(searchHistory);
 // my api key
 let apiKey = "d47e097f5d363d7b0b0a962b12e789dc";
 
-
 // event listener button to capture user input to local storage
 searchBtn.on('click', function (event) {
     event.preventDefault();
     let userInput = $("#userInput").val().toLowerCase();
+    // console.log(searchBtn);
+    // // city input
+    // let cityInput = $('#userInput').val();
+    // cities.push(cityInput);
+    // //store city input in local storage
+    // localStorage.setItem('cities', JSONß.stringify(cities));
     searchCityCurrentWeather(userInput);
 });
 
@@ -35,9 +53,6 @@ function capitalizeWords(str) {
     return capWords.join(" ");
 }
 
-// on page load, check for existing city searches
-//add those as button to page
-// if the input is a new city, add to page
 function renderHistory(onLoad, newCity) {
     let searchHistoryEl = $("#searchHistory");
     if (onLoad) {
@@ -56,13 +71,11 @@ function renderHistory(onLoad, newCity) {
         searchBtn.addClass('search');
         liEl.append(searchBtn);
         searchHistoryEl.append(liEl)
-
     };
 };
 
 
 function searchCityCurrentWeather(city) {
-    // d47e097f5d363d7b0b0a962b12e789dc
     const endpoint = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
     fetch(endpoint)
         .then(function (turnToJson) {
@@ -70,7 +83,10 @@ function searchCityCurrentWeather(city) {
         })
         .then(function (response) {
             console.log(response);
-            cityEl.text(response.name)
+            cityEl.text(response.name + currentDay)
+            tempEl.text(response.main.temp + ' °F')
+            windEl.text(response.wind.speed + ' MPH')
+            humidityEl.text(response.main.humidity + ' %')
             searchOneCallAPI(response.coord.lat, response.coord.lon, city)
             // let h4El = $("<h4>");
             // h4El.text(response.name);
@@ -78,7 +94,7 @@ function searchCityCurrentWeather(city) {
         });
 };
 
-function searchOneCallAPI(lat, lon, cityPassThrough) {
+function searchOneCallAPI(lat, lon, city) {
     let endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     $.ajax({
         url: endpoint,
@@ -86,12 +102,28 @@ function searchOneCallAPI(lat, lon, cityPassThrough) {
     })
         .then(function (response) {
             console.log('One Call API Response: ', response);
-
-            if (!searchHistory.includes(cityPassThrough)) {
-                searchHistory.push(cityPassThrough);
+            let uvIndex = uviEl.text(response.current.uvi)
+            // let uvIndex = response.current.uvi;
+            if (!searchHistory.includes(city)) {
+                searchHistory.push(city);
                 localStorage.setItem("history", JSON.stringify(searchHistory));
-                renderHistory(false, cityPassThrough);
+                renderHistory(false, city);
             }
+            // uv index color
+            // if (uvIndex < 3) {
+            //     uviEl.addClass("favorable");
+            // } else if (uviIndex >= 3 && uviIndex < 6) {
+            //     uviEl.addClass("moderate");
+            // } else if (uviIndex > 6) {
+            //     uviEl.addClass("severe");
+            // }
+
+            // creating 5 days forecast, append to dashboard
+            for (let i = 0; i < response.daily.length[4]; i++) {
+                let pEl = $("<p>");
+
+            };
+
         });
 };
 
@@ -101,64 +133,3 @@ $(document).on('click', '.search', function (e) {
 
     searchCityCurrentWeather(cityValue);
 });
-
-// new function to do: create button from local storage
-//on load check local storage for city name as an array. then create buttons from them. make their onclick event button to make api call then display
-
-// let cities = getSavedCities();
-
-// function getSavedCities() {
-//     let cities = JSON.parse(localStorage.getItem('cities'));
-//     if (cities) {
-//         return cities;
-//     } else {
-//         return [];
-//     }
-// };
-
-
-// function searchHistory() {
-//     for (var i = 0; i < cities.length; i++) {
-//         // appending value of cities as a list to #searchHistory
-
-//         $("li").text(function (i) {
-//             return cities[i];
-//         });
-
-//         $('li').append('cities');
-//     }
-// };
-
-// // get lon and lat of a city name
-// let lastCity = cities[cities.length - 1];
-// let geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${lastCity},US&appid=4bcac4085f133666bc3803dc7ed2e35c
-// `;
-
-// fetch(geoUrl)
-//     .then(response => response.json())
-//     .then(data => getWeather(data[0].lon, data[0].lat));
-
-// //get weather based on city name search
-// function getWeather(lat, lon) {
-
-//     let weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=4bcac4085f133666bc3803dc7ed2e35c
-//     `;
-
-//     fetch(weatherUrl)
-//         .then(function (response) {
-//             console.log(response)
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         })
-// }
-
-
-// city name
-// date
-// icon representing weather condition
-// temperature
-//humidity
-//wind speed
-//uv index (with color indicating conditions: favorable, moderate, severe)
